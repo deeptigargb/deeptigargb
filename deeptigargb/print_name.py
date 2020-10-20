80b5264ac07910e5b8bfa2d25c59bb65997311be
Deepti Garg <deepti.garg@babylonhealth.com>
2020-10-20 17:09:09 +0100

    name

--- deeptigargb/print_name.py
+++ deeptigargb/print_name.py
@@ -1,2 +1,5 @@
-def nameplease(name):
-    print("Hey buddy! Your name is {} and my nnpame is Deepti. Welcome to my package".format(name))
+class print_name:
+    """ Welcome to my package, hope you enjoy. Keep rocking"""
+
+    def nameplease(name):
+        print("Hey buddy! Your name is {} and my nnpame is Deepti. Welcome to my package".format(name))
